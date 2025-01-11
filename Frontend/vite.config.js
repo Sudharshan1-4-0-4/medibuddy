cc9f97cd44dd6a394e1be3e878c2fd6e287ae545
Sudharshan Mothukuru <123439099+Sudharshan1-4-0-4@users.noreply.github.com>
2025-01-11 17:02:28 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Allows connections from any network interface
+    port: 3000,        // You can set this to 3000 or any other available port
+  }
 })
